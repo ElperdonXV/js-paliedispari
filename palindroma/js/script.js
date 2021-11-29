fe794279eef2df9946dc35b1b0eee60939bde2ab
ElperdonXV <davidemartinografico@gmail.com>
2021-11-29 14:21:18 +0100

    work on function

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,12 +1,16 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 //(prima senza funzione e poi con funzione)
-let parola = prompt('Inserisci una parola');
-let parolaSuddivisa = parola.split('');
-parolaSuddivisa = parolaSuddivisa.reverse();
-let inverso = parolaSuddivisa.join('');
-console.log(inverso);
-if (parola == inverso){
-    console.log( parola + " è una parola palindroma");
+
+function palindroma(parola){
+    let parola = prompt('Inserisci una parola');
+    let parolaSuddivisa = parola.split('');
+    parolaSuddivisa = parolaSuddivisa.reverse();
+    let inverso = parolaSuddivisa.join('');
+    let result = false;
+    if (parola == inverso){
+        result = true;
+    }
+    return result;
 }
-else console.log(parola + " non è una parola palindroma");
+
