5d96515fc78b261771e3412029804edc98993416
ElperdonXV <davidemartinografico@gmail.com>
2021-11-29 13:59:54 +0100

    work on parola

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,3 +1,6 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 //(prima senza funzione e poi con funzione)
+let parola = prompt('Inserisci una parola');
+let parolaSuddivisa = parola.split('');
+console.log(parolaSuddivisa);
